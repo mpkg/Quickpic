461884ed76b5b200ff8c17d181eac2e2a7025248
mpkg <mayank.gadodia87@gmail.com>
2013-09-16 19:39:51 +0530

    16/9

--- Scripts/QuickPic.js
+++ Scripts/QuickPic.js
@@ -30,7 +30,7 @@
 {
     function loadImage() {
         $(divQuickPic).width(imageWidth);
-        RetreiveImage();
+        retreivePictures(EntityName, RecordID);
         var image = $(divImgPreview);
         if (ImageText != '') {
             image.attr('src', ImageText);
@@ -40,7 +40,11 @@
         }
         image.attr('width', imageWidth);
         image.attr('height', height);
-        $(divImage).css('left', imageLeftPosition + 'px');
+        $(divImage).css('left', imageLeftPosition + 'px')
+                   .css('position', 'fixed')
+                   .css('top', '0px');
+        $(divFileSelect).css('position', 'relative')
+                        .css('top', (height - 0.45 * height) + 'px');
         toggleFileSelect();
     }
 
@@ -51,6 +55,85 @@
         }).mouseleave(function () {
             $(divFileSelect).fadeOut();
         });
+    }
+
+    function retreivePictures(entityName, recordId) {
+        var odataUri = ODATA_URL + "/" + ODATA_QUICKPICKDATA + "?";
+        var select = "unizap_ImageText,unizap_quickpicdataId,unizap_ImageNo,unizap_FacebookProfileURL";
+        var filter = "unizap_RecordGUID eq '" + recordId + "' and unizap_EntityName eq '" + entityName + "'";
+        odataUri += "$select = " + select;
+        odataUri += "&$filter = " + filter;
+        $.ajax({
+            async: false,
+            type: "GET",
+            contentType: "application/json; charset=utf-8",
+            datatype: "json",
+            url: odataUri,
+            beforeSend: function (XMLHttpRequest) {
+                //Specifying this header ensures that the results will be returned as JSON.
+                XMLHttpRequest.setRequestHeader("Accept", "application/json");
+            },
+            success: function (data, textStatus, XmlHttpRequest) {
+                if (data.d.results != null && data.d.results.length > 0) {
+                    ImageText = data.d.results[0].unizap_ImageText;
+                    QuickPicDataId = data.d.results[0].unizap_quickpicdataId;
+                }
+            },
+            error: function (XmlHttpRequest, textStatus, errorThrown) {
+                alert('OData Retrieve Failed: ' + odataUri + " ; Error – " + XmlHttpRequest.responseText);
+            }
+        });
+    }
+
+    function displayPicture(imageNo) {
+    }
+
+    function addPicture(files) {
+        var image = $(divImgPreview);
+        var reader;
+        try {
+            reader = new FileReader();
+        } catch (err) {
+            alert(HTML5AlertMsg);
+            return;
+        }
+
+        reader.onload = function (e) {
+            // e.target.result holds the DataURL which
+            // can be used as a source of the image:
+            if (e.target.result.length > max_file_size) {
+                alert(FileSizeBigMsg);
+                return;
+            }
+            image.attr('src', e.target.result);
+            image.attr('width', imageWidth);
+            image.attr('height', height);
+            $(divImage).css('left', imageLeftPosition + 'px');
+            var quickPicData = new Object();
+            quickPicData.unizap_JSImageText = e.target.result;
+            quickPicData.unizap_RecordGUID = RecordID;
+            quickPicData.unizap_EntityName = EntityName;
+            if (QuickPicDataId == '' || QuickPicDataId == null) {
+                createRecord(quickPicData);
+            }
+            else {
+                updateRecord(QuickPicDataId, quickPicData);
+            }
+            toggleFileSelect();
+        };
+
+        // Reading the file as a DataURL. When finished,
+        // this will trigger the onload function above:
+        reader.readAsDataURL(files[0]);
+    }
+
+    function deletePicture(imageNo) {
+    }
+
+    function changePicture(imageNo) {
+    }
+
+    function browse(currentIndex, direction) {
     }
 
     function deleteRecord(id) {
@@ -137,85 +220,6 @@
                 }
             }
         });
-    }
-
-    function retreivePictures(entityName, recordId) {
-        var odataUri = ODATA_URL + "/" + ODATA_QUICKPICKDATA + "?";
-        var select = "unizap_ImageText,unizap_quickpicdataId,unizap_ImageNo,unizap_FacebookProfileURL";
-        var filter = "unizap_RecordGUID eq '" + RecordID + "' and unizap_EntityName eq '" + EntityName + "'";
-        odataUri += "$select = " + select;
-        odataUri += "&$filter = " + filter;
-        $.ajax({
-            async: false,
-            type: "GET",
-            contentType: "application/json; charset=utf-8",
-            datatype: "json",
-            url: odataUri,
-            beforeSend: function (XMLHttpRequest) {
-                //Specifying this header ensures that the results will be returned as JSON.
-                XMLHttpRequest.setRequestHeader("Accept", "application/json");
-            },
-            success: function (data, textStatus, XmlHttpRequest) {
-                if (data.d.results != null && data.d.results.length > 0) {
-                    ImageText = data.d.results[0].unizap_ImageText;
-                    QuickPicDataId = data.d.results[0].unizap_quickpicdataId;
-                }
-            },
-            error: function (XmlHttpRequest, textStatus, errorThrown) {
-                alert('OData Retrieve Failed: ' + odataUri + " ; Error – " + XmlHttpRequest.responseText);
-            }
-        });
-    }
-
-    function displayPicture(imageNo) {
-    }
-
-    function addPicture(files) {
-        var image = $(divImgPreview);
-        var reader;
-        try {
-            reader = new FileReader();
-        } catch (err) {
-            alert(HTML5AlertMsg);
-            return;
-        }
-
-        reader.onload = function (e) {
-            // e.target.result holds the DataURL which
-            // can be used as a source of the image:
-            if (e.target.result.length > max_file_size) {
-                alert(FileSizeBigMsg);
-                return;
-            }
-            image.attr('src', e.target.result);
-            image.attr('width', imageWidth);
-            image.attr('height', height);
-            $(divImage).css('left', imageLeftPosition + 'px');
-            var quickPicData = new Object();
-            quickPicData.unizap_JSImageText = e.target.result;
-            quickPicData.unizap_RecordGUID = RecordID;
-            quickPicData.unizap_EntityName = EntityName;
-            if (QuickPicDataId == '' || QuickPicDataId == null) {
-                createRecord(quickPicData);
-            }
-            else {
-                updateRecord(QuickPicDataId, quickPicData);
-            }
-            toggleFileSelect();
-        };
-
-        // Reading the file as a DataURL. When finished,
-        // this will trigger the onload function above:
-        reader.readAsDataURL(files[0]);
-    }
-
-    function deletePicture(imageNo) {
-    }
-
-    function changePicture(imageNo) {
-    }
-
-    function browse(currentIndex, direction) {
     }
 
     function ConfigureAjaxLoading(text) {
