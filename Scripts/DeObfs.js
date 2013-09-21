24deb9a11c1ed28d5c3298e0a721d437683f823a
mpkg <mayank.gadodia87@gmail.com>
2013-09-21 16:36:26 +0530

    Obfuscate/Create Form/Try catch - check

--- Scripts/DeObfs.js
+++ Scripts/DeObfs.js
@@ -1,1 +1,27 @@
-﻿
+﻿//a
+{
+    displayPicture(images[0].unizap_ImageText);
+}
+
+//b
+{
+    displayPicture(images[index].unizap_ImageText);
+}
+
+//c
+{
+    displayPicture(images[displayPictureIndex].unizap_ImageText);
+}
+
+//d
+{
+    var quickPicData = new Object();
+    quickPicData.unizap_JSImageText = e.target.result;
+    quickPicData.unizap_RecordGUID = RecordID;
+    quickPicData.unizap_EntityName = EntityName;
+    try {
+        createRecord(quickPicData);
+    }
+    catch (e) {
+    }
+}  
